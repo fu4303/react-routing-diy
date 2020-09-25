14942c91dba70c48cb32d78667bb0428ffa4d341
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-09-25 11:58:22 +0100

    Delete CRA crap and write starting code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [page, setPage] = React.useState(window.location.pathname);
+
+  const navigate = (event) => {
+    // stop the normal browser navigation
+    event.preventDefault();
+    window.history.pushState(null, null, event.target.pathname);
+    setPage(event.target.pathname);
+  };
+
+  React.useEffect(() => {
+    const onHistoryChange = () => {
+      setPage(window.location.pathname);
+    };
+    window.addEventListener("popstate", onHistoryChange);
+    return () => window.removeEventListener("popstate", onHistoryChange);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav>
+        <a href="/" onClick={navigate}>
+          Home
         </a>
-      </header>
+        <a href="/about" onClick={navigate}>
+          About
+        </a>
+        <a href="/contact" onClick={navigate}>
+          Contact
+        </a>
+      </nav>
+      <main>
+        {page === "/" && <Home />}
+        {page === "/about" && <About />}
+        {page === "/contact" && <Contact />}
+      </main>
     </div>
   );
+}
+
+function Home() {
+  return <h1>Home page</h1>;
+}
+
+function About() {
+  return <h1>About page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact page</h1>;
 }
 
 export default App;
